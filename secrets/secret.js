681f6ef8a2e6881d8cb3da2deb530a6291a963f8
TheBoringDude <iamcoderx@gmail.com>
2021-04-10 21:06:22 +0800

    init

---
+++ secrets/secret.js
@@ -0,0 +1,5 @@
+const {customAlphabet} = require('nanoid')
+
+const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*(-_=+)', 100)
+
+console.log(nanoid())
